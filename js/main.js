41fd9d3514c56ffcc68017a6c31d7cca517f7152
vasifismayilov <ismayilov2265@gmail.com>
2022-04-19 20:02:22 +0400

    MobileSearch

---
+++ js/main.js
@@ -0,0 +1,10 @@
+
+
+const price = document.querySelector('#price');
+const output = document.querySelector('.price-output');
+
+output.textContent = price.value +" AZN";
+
+price.addEventListener('input', function() {
+  output.textContent = price.value + " AZN";
+});
